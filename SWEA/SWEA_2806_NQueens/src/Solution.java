819413fbd59114d9dbc8ddff26eac7ab18292073
tmdwls1015 <55172961+tmdwls1015@users.noreply.github.com>
2023-01-20 09:19:20 +0900

    Solving 2806

---
+++ SWEA/SWEA_2806_NQueens/src/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[][] map = new int[N][N];
+			
+			
+		}
+	}
+
+}
